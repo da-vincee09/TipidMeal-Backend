ec1ecce254472fcee1c5f2965f540aee42e829b1
da-vincee09 <rodinirosario@gmail.com>
2026-09-17 08:51:03 +0800

    perf: use QueuePool instead of NullPool to reuse connections to Supabase pooler

--- core/database.py
+++ core/database.py
@@ -1,18 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import NullPool
 from core.config import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    poolclass=NullPool,
-    pool_pre_ping=True
+    pool_size=3,
+    max_overflow=5,
+    pool_pre_ping=True,
+    pool_recycle=1800,
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 from shared.database import models
